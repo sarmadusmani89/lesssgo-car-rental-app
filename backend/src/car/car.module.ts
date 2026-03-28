09bb9df2eb6bcd55cc60e1b86bf6641af88fad60
Sarmad Usmani <125732469+sarmadusmani89@users.noreply.github.com>
2026-03-28 12:46:41 +0500

    feat: initialize car module with controller, service, and cloudinary providers

--- backend/src/car/car.module.ts
+++ backend/src/car/car.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CarService } from './car.service';
 import { CarController } from './car.controller';
-import { PrismaService } from '../lib/prisma.service';
 import { CloudinaryService } from '../lib/cloudinary.service';
 import { CloudinaryProvider } from '../lib/cloudinary.provider';
 
 @Module({
   controllers: [CarController],
-  providers: [CarService, PrismaService, CloudinaryService, CloudinaryProvider],
+  providers: [CarService, CloudinaryService, CloudinaryProvider],
   exports: [CarService],
 })
 export class CarModule { }
