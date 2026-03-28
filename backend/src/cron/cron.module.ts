105352e8873d4fca74ec928a0434ea9e500c3e6d
Sarmad Usmani <125732469+sarmadusmani89@users.noreply.github.com>
2026-03-28 12:46:58 +0500

    feat: create CronModule to handle automated booking reminders

--- backend/src/cron/cron.module.ts
+++ backend/src/cron/cron.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BookingReminderCron } from './bookingReminder.cron';
 import { EmailModule } from '../email/email.module';
-import { PrismaService } from '../lib/prisma.service';
-
 @Module({
     imports: [
         EmailModule,
     ],
-    providers: [BookingReminderCron, PrismaService],
+    providers: [BookingReminderCron],
 })
 export class CronModule { }
--- backend/src/dashboard/dashboard.module.ts
+++ backend/src/dashboard/dashboard.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 import { DashboardController } from './dashboard.controller';
-import { PrismaService } from '../lib/prisma.service';
-
 @Module({
-    providers: [DashboardService, PrismaService],
+    providers: [DashboardService],
     controllers: [DashboardController],
 })
 export class DashboardModule { }
